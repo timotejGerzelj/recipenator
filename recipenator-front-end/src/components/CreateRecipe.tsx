d80f9ce2780c941c661ca3cbb2f92c6229ae4452
timotejGerzelj <timotej.gerzelj@gmail.com>
2023-07-31 12:06:51 +0200

    feat: Added handler outlines and basic create recipe form

--- recipenator-front-end/src/components/CreateRecipe.tsx
+++ recipenator-front-end/src/components/CreateRecipe.tsx
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, Fragment, useState } from 'react';
+import { useForm, useFieldArray } from "react-hook-form";
+
+type FormValues = {
+    recipeName: '',
+    recipeDescription: '',
+    recipeIngredients: { name: string }[],
+  };
 
 const CreateRecipe = () => {
-    return (
-        <div>
-            <h2>Describe new recipe</h2>
-            
-        </div>
-    );
+    const onSubmit = (data: any) => console.log(data);
     
+    const { register, handleSubmit, control, watch} = useForm<FormValues>();
+    const { fields, append } = useFieldArray({
+        control,
+        name: "recipeIngredients"
+      });
+      const watchFieldArray = watch("recipeIngredients");
+      const controlledFields = fields.map((field, index) => {
+        console.log(field);
+        console.log(index);
+        return {
+          ...field,
+          ...watchFieldArray[index]
+        };
+      });
+
+
+
+    return <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+            Recipe Name:
+        </label>
+        <input 
+            {...register("recipeName")} placeholder='What is the recipe called?'
+        />
+        <label>
+            Description:
+        </label>
+        <textarea 
+            {...register("recipeDescription")}
+            placeholder='How can the recipe be made?'
+        />
+        <label>
+            Ingredients:
+        </label>
+
+        {controlledFields.map((field, index) => {
+          return <input {...register(`recipeIngredients.${index}.name` as const)} />;
+        })}
+
+        
+        {/* Append button to add new ingredients */}
+        <button type="button"
+        onClick={() =>
+            append({
+              name: ""
+            })
+          }>Append
+        </button>
+        <button type="submit">Submit Recipe</button>
+       </form>    
 }
 
 export default CreateRecipe;
